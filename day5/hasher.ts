ee4bc5622d1e45a77109096f0b7cbd3c39e3dee4
Peter Seale <peter@pseale.com>
2016-12-12 01:28:01 -0600

    refactor day 6

--- day5/hasher.ts
+++ day5/hasher.ts
@@ -1,14 +1,14 @@
 "use strict"
 
 import * as _ from "lodash"
-var md5 = require("md5")
+const md5 = require("md5")
 
 function solvePartA() {
   let i = 1
   let password = ""
   while (password.length < 8) {
-    var result = md5(`ugkcyxxp${i}`)
-    var md5Hash = String(result)
+    const result = md5(`ugkcyxxp${i}`)
+    const md5Hash = String(result)
     if (md5Hash.substr(0, 5) === "00000") {
       password += md5Hash.substr(5, 1)
     }
