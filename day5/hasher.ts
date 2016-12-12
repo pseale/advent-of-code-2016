20bd1cfe67fd7cec2bf3fc090c069f5d6be94710
Peter Seale <peter@pseale.com>
2016-12-12 01:45:21 -0600

    solve day 5 part B

--- day5/hasher.ts
+++ day5/hasher.ts
@@ -20,5 +20,37 @@ function solvePartA() {
   }
 }
 
+function fillInPassword(password : string[], position : number, character : string) : void {
+  if (password[position] === null) {
+    password[position] = character
+  }
+}
+
+function solvePartB() {
+  let i = 1
+  let password = [null, null, null, null, null, null, null, null]
+  while (password.some(x => x === null)) {
+    const result = md5(`ugkcyxxp${i}`)
+    const md5Hash = String(result)
+    if (md5Hash.substr(0, 5) === "00000") {
+      var sixthChar = md5Hash.substr(5, 1)
+      var seventhChar = md5Hash.substr(6, 1)
+      if (sixthChar >= "0" && sixthChar <= "7") {
+        fillInPassword(password, Number(sixthChar), seventhChar)
+      }
+    }
+
+    i++
+    if (i % 100000 === 0) {
+      console.log(`[${password.join("")}] Hashed ${i} hashes...`)
+    }
+  }
+
+  return password.join("")
+}
+
 var partAPassword = solvePartA()
 console.log(`Part A: Final password is: ${partAPassword}`)
+
+var partBPassword = solvePartB()
+console.log(`Part B: Final password is: ${partBPassword}`)
